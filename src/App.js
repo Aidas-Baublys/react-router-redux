dc1d9cf9c78e8a260bbb64cb2a283247e171942e
Aidas Baublys <aidas.baublys.darbo@gmail.com>
2021-08-06 12:38:07 +0300

    Update styles, add button with onClick event handler and do minor refactor.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 
-import './App.css';
-
 function App() {
   return (
     <div className="App">
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,10 @@
 function Home() {
+  const onClick = () => console.log("Fuck yeah!");
+
   return (
     <div className="home">
       <h2>Home</h2>
+      <button onClick={onClick}>Oh, yeah...</button>
     </div>
   );
 }
