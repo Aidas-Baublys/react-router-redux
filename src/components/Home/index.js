c3754de7731d9458b11007376cc93f4262885230
Aidas Baublys <aidas.baublys.darbo@gmail.com>
2021-08-07 18:24:32 +0300

    Implement json server.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import BlogList from './../BlogList/index';
 
 function Home() {
-  const [blogs, setBlogs] = useState([
-    { title: "Tits", body: "Tits are great", author: "Jo", id: 1 },
-    { title: "Boobs", body: "Awesome boobs", author: "Po", id: 2 },
-    { title: "Breasts", body: "Drooly drool drool", author: "Lo", id: 3 },
-  ]);
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then(res => res.json())
+      .then(data => setBlogs(data));
+  }, []);
 
   const deleteBlog = id => {
     setBlogs(blogs.filter(blog => blog.id !== id));
